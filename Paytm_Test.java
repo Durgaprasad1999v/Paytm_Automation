d957f0cb917f8324c7517cc05da69e3a89c21d3c
Durgaprasad1999v <132452056+Durgaprasad1999v@users.noreply.github.com>
2023-06-26 14:22:00 +0530

    Update Paytm_Test.java

--- Paytm_Test.java
+++ Paytm_Test.java
@@ -27,19 +27,19 @@ public class Paytm_Test {
 	{
 		ChromeOptions options=new ChromeOptions();
 		options.addArguments("--disable-notifications");
-		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\vijay\\\\Downloads\\\\chromedriver_win32 (1)\\\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vijay\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver(options);
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		JavascriptExecutor js=(JavascriptExecutor)driver;
 		driver.get("https://www.paytm.com");
 		driver.manage().window().maximize();
-		JavascriptExecutor js=(JavascriptExecutor)driver;
 		js.executeScript("window.scrollBy(0,1000)","");
-		driver.findElement(By.xpath("/html/body/div/section[4]/div/div/div/div[2]")).click();
-		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[1]/input")).sendKeys("Delhi");
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		driver.findElement(By.xpath("//*[@id=\"app\"]/section[4]/div/div/div/div[2]")).click();
+		driver.findElement(By.xpath("(//*[@id=\"text-box\"])[1]")).sendKeys("Delhi");
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("//*[@id=\"flightsBookingForm\"]/div[2]/div[2]/div[1]/div[2]/div/div/div[2]/div/div/div[1]/div[2]")).click();
 		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div[4]/div[1]/div[1]/input")).sendKeys("Bengaluru");
+		driver.findElement(By.xpath("(//*[@id=\"text-box\"])[2]")).sendKeys("Bengaluru");
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("//*[@id=\"flightsBookingForm\"]/div[2]/div[4]/div[1]/div[2]/div/div/div[2]/div/div/div[1]/div[2]")).click();
 		Thread.sleep(2000);
@@ -55,7 +55,7 @@ public class Paytm_Test {
 		js.executeScript("window.scrollBy(0,400)", "");
 		Thread.sleep(2000);
 		Actions action=new Actions(driver);
-		WebElement ele=driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[6]/div[1]/div[9]/a"));
+		WebElement ele=driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/div/div[1]/div[5]/div[1]/div[9]/a"));
 		action.moveToElement(ele).build().perform();
 		Thread.sleep(2000);
 		ele.click();
@@ -70,10 +70,10 @@ public class Paytm_Test {
 		Thread.sleep(2000);
 		js.executeScript("window.scrollBy(0,1000)", "");
 		Thread.sleep(2000);
-		driver.findElement(By.cssSelector("#flights-review-traveller-details > div:nth-child(2) > div:nth-child(2) > div > div > a")).click();
+		driver.findElement(By.xpath("//*[@id=\"flights-review-traveller-details\"]/div[2]/div[2]/div/div/a")).click();
 		Thread.sleep(2000);
 		driver.switchTo().frame(0);
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/span/div")).click();
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div/span/div/div")).click();
 		Scanner sc=new Scanner(System.in);
 		sc.nextLine();
 		Thread.sleep(2000);
@@ -88,13 +88,22 @@ public class Paytm_Test {
 		js.executeScript("window.scrollBy(0,700)", "");
 		driver.findElement(By.xpath("//*[@id=\"flights-review-ancillary\"]/div[2]/div/div[1]/div/div[2]/div[1]")).click();
 		Thread.sleep(3000);
-		List<WebElement> list=driver.findElements(By.className("_2AV5"));
-		ArrayList<String> arr=new ArrayList<String>();
-		for(int i=0;i<list.size();i++)
+		List<WebElement> Flights=driver.findElements(By.xpath("//*[@id=\"flights-review-ancillary\"]/div[2]/div/div[1]/div[1]/div"));
+		for(int i=0;i<Flights.size();i++)
 		{
-			System.out.println(list.get(i).getText());
+			WebElement element=driver.findElement(By.xpath("//*[@id=\"flights-review-ancillary\"]/div[2]/div/div[1]/div[1]/div[@id="+i+"]"));
+			element.click();
+			List<WebElement> list=driver.findElements(By.className("_2AV5"));
+			ArrayList<String> arr=new ArrayList<String>();
+			System.out.println("Flight:  "+i);
+			for(int j=0;j<list.size();j++)
+			{
+				System.out.println(list.get(j).getText());
+				
+			}
+			Thread.sleep(2000);
 		}
-		String[] ans=arr.toArray(new String[arr.size()]);
+		
 		
 		
 		
